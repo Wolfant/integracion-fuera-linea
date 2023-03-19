fe73c27433d5bf726773e65c58550eb9a3f1c108
Antonio Insuasti <efainsua@pichincha.com>
2023-03-19 00:28:12 -0500

    create mvn proyect with camel and springboot

---
+++ offline-integration/src/main/java/ec/insuasti/ups/homeworks/offlineintegration/OfflineIntegrationApplication.java
@@ -0,0 +1,13 @@
+package ec.insuasti.ups.homeworks.offlineintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflineIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfflineIntegrationApplication.class, args);
+	}
+
+}
---
+++ offline-integration/src/test/java/ec/insuasti/ups/homeworks/offlineintegration/OfflineIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.insuasti.ups.homeworks.offlineintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflineIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
